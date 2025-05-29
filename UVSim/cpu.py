80df532490cba4065a41e5eb4ccf8a5709ef8751
Justin Nielson <justin.nielson@canyonwall.com>
2025-05-29 12:19:03 -0600

    Moved methods for running program into cpu.py, implemented basic logic for running commands

--- UVSim/cpu.py
+++ UVSim/cpu.py
@@ -1,4 +1,68 @@
+MEMORY_SIZE = 100
+
+
+
+
+def temp_add(num1, num2):
+    return num1 + num2
+
+
+
+operations = {
+    "10": {"method": temp_add, "store": "accumulator"},
+}
+
+
 
 class CPU:
-    def run_command(command):
-        print(command)
+    def __init__(self):
+        self.memory = [0] * MEMORY_SIZE
+        self.accumulator = 0
+        self.instruction_counter = 0
+        self.running = True
+
+    def load_program(self, filename: str) -> None:
+        """Load the commands from specified file into memory
+        Args:
+            filename: txt file location to load commands from
+        """
+        try:
+            with open(filename, 'r') as file:
+                for i, line in enumerate(file):
+                    line = line.strip()
+                    if not line or line.startswith('#'):
+                        continue
+                    if i >= MEMORY_SIZE:
+                        raise MemoryError("Program exceeds memory limit.")
+                    
+                    #TODO check if line is a valid instruction
+                    if False:
+                        raise ValueError()
+
+
+                    self.memory[i] = int(line)
+        except FileNotFoundError:
+            print(f"File not found: {filename}")
+            exit(1)
+        except ValueError:
+            print(f"Invalid instruction in file: {line}")
+            exit(1)
+    
+    def run_program(self, filename: str):
+        self.load_program()
+        while True:
+             instruction = str(self.memory[self.instruction_counter])
+             negative = True if "-" in instruction else False
+             op_code = instruction[-4:-2]
+             argument = instruction[-2:]
+
+             operation = operations[op_code]
+             args = [argument] if operations["args"] else []
+             value = operation["method"](*args)
+             match operation["store"]:
+                 case "accumulator":
+                     self.accumulator = value
+                 case "memory":
+                     self.memory[args] = value
+
+             self.instruction_counter += 1
--- uvsim.py
+++ uvsim.py
@@ -1,52 +1,12 @@
 
 from UVSim.cpu import CPU
 
-MEMORY_SIZE = 100
-
 class UVSim:
-    def __init__(self):
-        self.memory = [0] * MEMORY_SIZE
-        self.accumulator = 0
-        self.instruction_counter = 0
-        self.running = True
-
-    def load_program(self, filename: str) -> None:
-        """Load the commands from specified file into memory
-        Args:
-            filename: txt file location to load commands from
-        """
-        try:
-            with open(filename, 'r') as file:
-                for i, line in enumerate(file):
-                    line = line.strip()
-                    if not line or line.startswith('#'):
-                        continue
-                    if i >= MEMORY_SIZE:
-                        raise MemoryError("Program exceeds memory limit.")
-                    
-                    #check if line is a valid instruction
-                    if False:
-                        raise ValueError()
-
-
-                    self.memory[i] = int(line)
-        except FileNotFoundError:
-            print(f"File not found: {filename}")
-            exit(1)
-        except ValueError:
-            print(f"Invalid instruction in file: {line}")
-            exit(1)
-    
-    def run_program(self):
-        self.load_program()
+    def run_program(filename: str):
         cpu = CPU()
-
-
-        
-
+        cpu.run_program(filename)
 
 
 if __name__ == "__main__":
-
     sim = UVSim()
     sim.run_program()
